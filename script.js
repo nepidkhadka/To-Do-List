4bd476ff8a5daaef6b90a76969e7ffbdd7219a1a
Dipen Khadka <dipenkhadka191@gmail.com>
2023-04-16 16:40:48 +0545

    -- Added Task are Set to be Listed

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// console.log("Hello");
-let inputdata = document.querySelector('input').value;
-console.log(inputdata);
-
-
+let inputdata = document.querySelector("input");
 
 document.querySelector("#btn").onclick = function(){
-    if(inputdata.length === 0){
+    if(inputdata.value.length == 0){
         alert("Please Enter Your Task");
         console.log(inputdata);
-    }
+    }else{
+        document.querySelector("#tasks").innerHTML += `<div class="individual-task">${document.querySelector("input").value}</div>`;
+        inputdata.value= "";
+
+    }   
 }

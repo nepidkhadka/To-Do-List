c08175cc35c1aff6205567c57c8d2d2316119944
Dipen Khadka <dipenkhadka191@gmail.com>
2023-04-16 17:21:28 +0545

    -- Task Adding Feature -- Click & Enter Event Listener Added In JS

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let inputdata = document.querySelector("input");
+let mybtn = document.querySelector("#btn");
 
-document.querySelector("#btn").onclick = function(){
-    if(inputdata.value.length == 0){
-        alert("Please Enter Your Task");
-        console.log(inputdata);
-    }else{
-        document.querySelector("#tasks").innerHTML += `<div class="individual-task">${document.querySelector("input").value}</div>`;
-        inputdata.value= "";
+const addfunction = (event) => {
+  if (event.keyCode == 13 || event.type === "click") {
+    if (inputdata.value.length == 0) {
+      alert("Please Enter Your Task");
+    } else {
+      document.querySelector(
+        "#tasks"
+      ).innerHTML += `<div class="individual-task">${
+        document.querySelector("input").value
+      }</div>`;
+      inputdata.value = "";
+    }
+  }
+};
 
-    }   
-}
+inputdata.addEventListener("keypress", addfunction);
+mybtn.addEventListener("click", addfunction);

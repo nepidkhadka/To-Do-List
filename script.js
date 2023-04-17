3f81cf8d3e450de18009ef019e81de634dab7f50
Dipen Khadka <dipenkhadka191@gmail.com>
2023-04-17 21:12:19 +0545

    Delete Button Functionality Added and Made Some UI Chnages

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let inputdata = document.querySelector("input");
 let mybtn = document.querySelector("#btn");
 
+
 const addfunction = (event) => {
   if (event.keyCode == 13 || event.type === "click") {
     if (inputdata.value.length == 0) {
@@ -8,13 +9,27 @@ const addfunction = (event) => {
     } else {
       document.querySelector(
         "#tasks"
-      ).innerHTML += `<div class="individual-task">${
+      ).innerHTML += 
+      `<div class="task-list">
+      <div class="individual-task">${
         document.querySelector("input").value
-      }</div>`;
+      }</div>
+      <div class="deletebtn"><i class="fa-solid fa-trash"></i></div>
+      </div>
+      `;
       inputdata.value = "";
+      const delete_task = document.querySelectorAll(".deletebtn");
+      console.log(delete_task);
+      delete_task.forEach((btn) =>{
+      console.log(btn);
+        btn.addEventListener("click", ()=>{
+          btn.parentNode.remove();
+        })
+      })
     }
   }
 };
 
 inputdata.addEventListener("keypress", addfunction);
 mybtn.addEventListener("click", addfunction);
+
